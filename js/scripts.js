c43f00195287ebbc1f89ffdbd01906189b5614c7
Revanth <revkrock@gmail.com>
2019-09-01 08:37:01 -0400

    deleted unused files; reformatted; update content; typos

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,22 @@ var isVisible = function(el) {
 }
 
 var about = document.getElementById("about");
+var experience = document.getElementById("experience");
 var projects = document.getElementById("projects");
 var social = document.getElementById("social");
 
 var aboutDone = false;
+var experienceDone = false;
 var projectsDone = false;
 var socialDone = false;
 
 var animateOnScroll = function() {
   if(aboutDone === false && isVisible(about) === true) {
     about.classList.add("fadeIn");
+  }
+
+  if(experienceDone === false && isVisible(experience) === true) {
+    experience.classList.add("fadeIn");
   }
 
   if(projectsDone === false && isVisible(projects) === true) {
@@ -25,7 +31,7 @@ var animateOnScroll = function() {
     social.classList.add("fadeIn");
   }
 
-  if(aboutDone === true && projectsDone === true && socialDone === true) {
+  if(aboutDone === true && experienceDone === true && projectsDone === true && socialDone === true) {
     window.removeEventListener("scroll", animateOnScroll);
   }
 }
